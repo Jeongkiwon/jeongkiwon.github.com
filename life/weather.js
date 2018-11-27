dfbf67f0cb0e1dcb6828f0dcd7c2d1d1c2dd2bde
“Dg” <jeong214132@naver.com>
2018-11-27 23:01:02 +0900

    add todo list

---
+++ life/weather.js
@@ -0,0 +1,46 @@
+const API_KEY="a0f0936f343bf78fa6a220dcdb95f17d";
+const COORDS="coords";
+
+function getWeather(lat,lon){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`);
+}
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position){
+    console.log("ss");
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj={
+        latitude,
+        longitude,
+    };
+    saveCoords(coordsObj);
+    getWeather(latitude,longitude);
+}
+
+function handleGeoError(position){
+    console.log("Can access geo location");
+}
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSucces,handleGeoError);
+}
+
+function loadCoords(){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords===null){
+        askForCoords();
+    }else{
+        const parsedCoords=JSON.parse(loadedCoords)
+        //getWeather(parsedCoords.latitude, parsedCoords.longitude);
+    }
+}
+
+function init(){
+    loadCoords();
+}
+
+init();
